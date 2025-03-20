deb8b357b30b5ca9228296fad38a39c9051ebee9
nineeko <gkxm1013@naver.com>
2025-03-20 14:47:12 +0900

    25.03.20 pm 2:46

--- chap05-aop-lecture-source/src/main/java/com/neeko/section01/aop/ContextConfiguration.java
+++ chap05-aop-lecture-source/src/main/java/com/neeko/section01/aop/ContextConfiguration.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
+/* proxyTargetClass = ture : Proxy 객체 생성의 방식으로 CGLib 방식을 사용
+* target object가 class인 경우에도 proxy 객체 생성 가능 */
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 public class ContextConfiguration {
 }
---
+++ chap05-aop-lecture-source/src/main/java/com/neeko/section03/proxy/subsection02/cglib/Application.java
@@ -0,0 +1,23 @@
+package com.neeko.section03.proxy.subsection02.cglib;
+
+import com.neeko.section03.proxy.common.OhgiraffersStudent;
+import com.neeko.section03.proxy.common.Student;
+import org.springframework.cglib.proxy.Enhancer;
+
+import java.lang.reflect.Proxy;
+
+public class Application {
+    public static void main(String[] args) {
+        /* 2. CGLib Proxy 방식
+         * Target Object의 타입이 Class 여도 가능하다.
+         * */
+
+        OhgiraffersStudent ohgiraffersStudent = new OhgiraffersStudent();
+        Handler handler = new Handler(ohgiraffersStudent);
+
+        OhgiraffersStudent proxy
+                = (OhgiraffersStudent) Enhancer.create(OhgiraffersStudent.class, handler);
+        proxy.study(12);
+
+    }
+}
